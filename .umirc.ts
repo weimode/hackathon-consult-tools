93298fb79aa6ca13038c84229958f4fcc7f0dfdb
sura <xrwei@vds.kefa.com>
2021-01-14 13:55:55 +0800

    initial.

---
+++ .umirc.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'umi';
+import postcssPx2vw from 'postcss-px-to-viewport';
+
+export default defineConfig({
+  hash: true,
+  history: {
+    type: 'hash',
+  },
+  dva: {
+    hmr: true,
+  },
+  antd: {},
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+  extraPostCSSPlugins: [
+    postcssPx2vw({
+      viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+      viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+    }),
+  ],
+});
---
+++ deploy.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const spawn = require('cross-spawn');
+const CLI = require('clui');
+const simpleGit = require('simple-git');
+
+const Spinner = CLI.Spinner;
+
+const ora = require('ora');
+
+async function deploy() {
+  // const args = process.argv;
+  // const envArg = args.find((item) => item.indexOf('env=') === 0);
+  // const env = envArg ? envArg.split('=')[1] : envArg;
+  // const buildArg = env ? `:${env}` : '';
+  ora('部署开始...').info();
+  spawn.sync('yarn', ['build'], { stdio: 'inherit' });
+  ora('build 完成').info();
+  const status = new Spinner('初始化git...');
+  status.start();
+  const git = simpleGit({
+    baseDir: path.resolve(__dirname, 'dist'),
+    binary: 'git',
+  });
+  try {
+    await git
+      .init()
+      .checkoutLocalBranch('develop')
+      .add('./*')
+      .commit('auto commit')
+      .addRemote(
+        'origin',
+        'http://gitlab.apps.test.datadrivecloud.com/hackthon/front.git',
+      )
+      .push(['origin', 'develop', '--force']);
+    status.stop();
+    ora('build push完成..').info();
+  } catch (err) {
+    console.log(err.toString());
+    status.stop();
+  }
+}
+
+deploy();
